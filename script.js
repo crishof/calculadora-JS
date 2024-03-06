c7c777a777f87c3301c47e9eedb5a8398b38e12b
crishof <cristianhof@gmail.com>
2024-03-06 15:53:56 +0100

    add styles

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function agregar(valor){
-    document.getElementById('pantalla').value += valor;
+function agregar(valor) {
+  document.getElementById("pantalla").value += valor;
 }
 
-function calcular(){
-
-    const valorPantalla =document.getElementById('pantalla').value
-    const resultado = eval(valorPantalla)
-    document.getElementById('pantalla').value = resultado
+function calcular() {
+  const valorPantalla = document.getElementById("pantalla").value;
+  const resultado = eval(valorPantalla);
+  document.getElementById("pantalla").value = resultado;
 }
 
-function borrar(){
-    document.getElementById('pantalla').value = '';
-};
+function borrar() {
+  document.getElementById("pantalla").value = "";
+}
